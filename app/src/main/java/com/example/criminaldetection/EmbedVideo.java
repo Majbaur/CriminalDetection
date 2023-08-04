23312fe3dcc6c479db1b185edf6a657450fc1236
majbaurkhan <mystudy7980@gmai.com>
2023-08-04 22:36:06 +0600

    commit for others file

---
+++ app/src/main/java/com/example/criminaldetection/EmbedVideo.java
@@ -0,0 +1,39 @@
+package com.example.criminaldetection;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.criminaldetection.databinding.ActivityEmbedVideoBinding;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+
+public class EmbedVideo extends AppCompatActivity {
+
+    ActivityEmbedVideoBinding activityEmbedVideoBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        activityEmbedVideoBinding = ActivityEmbedVideoBinding.inflate(getLayoutInflater());
+        setContentView(activityEmbedVideoBinding.getRoot());
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        activityEmbedVideoBinding.actionbarVideo.actionTitle.setText("Video Player");
+
+
+        activityEmbedVideoBinding.youtubeVideo.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "cUWTvbn7eCI";
+                youTubePlayer.cueVideo(videoId, 0);
+            }
+        });
+
+
+    }
+}
